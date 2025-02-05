77e726db34f2dc21425a4768702251922c0da7ee
Maksim Andreev <andrianov_maksim@outlook.com>
2025-02-05 10:56:18 +0300

    add card transaction

--- src/card_transactions.py
+++ src/card_transactions.py
@@ -1,1 +1,7 @@
 # This is where the functions that process operations on the card are located. 
+from src.utils import conversion_xlsx_to_object
+
+
+def make_a_top_transaction():
+    conversion_xlsx_to_object("data/operations.xlsx")
+    pass
